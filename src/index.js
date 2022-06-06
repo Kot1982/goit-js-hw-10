53dc090c11572beae318ddacba825bb2c9507142
Kostiantyn Oliinyk <94046694+Kot1982@users.noreply.github.com>
2022-06-06 21:03:45 +0300

    update start file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { debounce } from 'lodash';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
